3bd001b7dc050284a84682a217d79f911d8febc5
Billy Fowks <fowkswe@gmail.com>
2021-09-01 16:56:21 -0500

    updated logo

--- components/graphics/Logo.vue
+++ components/graphics/Logo.vue
@@ -1,11 +1,6 @@
 <template>
-  <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path d="M16 27C22.0751 27 27 22.0751 27 16H32C32 24.8366 24.8366 32 16 32C7.16344 32 0 24.8366 0 16H5C5 22.0751 9.92487 27 16 27Z" fill="#850AFF"/>
-    <g style="mix-blend-mode:multiply">
-      <path d="M26 0L19 16H14L21 0H26Z" fill="#850AFF"/>
-    </g>
-    <g style="mix-blend-mode:multiply">
-      <path d="M9 8L13 16H18L14 8H9Z" fill="#850AFF"/>
-    </g>
-  </svg>
+    <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M16 27C22.0751 27 27 22.0751 27 16H32C32 24.8366 24.8366 32 16 32C7.16344 32 0 24.8366 0 16H5C5 22.0751 9.92487 27 16 27Z" fill="#850AFF" />
+        <path d="M19 16L26 0H21L15.8667 11.7333L14 8H9L13 16H19Z" fill="#850AFF" />
+    </svg>
 </template>
