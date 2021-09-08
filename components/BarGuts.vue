6fa69c8f32fbb3ae2f40ab9731e9c91197a226f2
Max Burnside <maxburnside@gmail.com>
2021-09-08 09:04:36 -0700

    bar-group renamed to bar_group, removed unused footer styling, comment deleted

--- components/BarGuts.vue
+++ components/BarGuts.vue
@@ -3,7 +3,7 @@
     .chart(:class='orientation')
       .legend
         .legendy % of Users
-      .bar-group(v-for='(stat, i) in stats' :class='barChartCount' v-if='showBar(stat)' )
+      .bar_group(v-for='(stat, i) in stats' :class='barChartCount' v-if='showBar(stat)' )
         .channel
           .bar(:class='statClass(i)' :style='barSize(stat.count)')
             .stat 
@@ -88,7 +88,7 @@ export default {
     font-family: 'Inter-Black', sans-serif
   .horizontal
     .chart
-      .bar-group
+      .bar_group
         &:not(:last-child)
           margin-bottom: 16px
       .bar
@@ -124,7 +124,6 @@ export default {
     .chart
       display: flex
       justify-content: space-between
-      // height: 260px
       position: relative
       .many_bars > .channel .bar
         border-radius: 11px 11px 0 0
@@ -153,7 +152,7 @@ export default {
         margin: 0 40px
         .stat
           font-size: 34px
-      .bar-group
+      .bar_group
         font-family: 'Inter-Black', sans-serif
         display: flex
         flex-direction: column
@@ -199,72 +198,8 @@ export default {
       text-align: center
       margin-bottom: 20px
 
-  .share
-    stroke: blue !important
-    &:hover
-      stroke: red !important
-  .footer
-    display: inline-flex
-    margin-bottom: 16px
-    width: 100%
-    height: 24px
-    align-items: center
-    justify-content: space-between
-    .company_logo
-      max-width: 192px
-      height: 24px
-      margin-right: 8px
-      &::after
-        content: ''
-        height: 16px
-        width: 1px
-        background-color: hsl(200, 16%, 88%)
-        position: relative
-        left: 8px
-        display: inline-block
-        top: 0
-      img
-        height: 24px
-    .ue_logo
-      width: 120px
-    .company_logo, .ue_logo
-      display: flex
-      align-items: center
-    svg
-      justify-content: center
-      align-items: center
-      max-height: 24px
-      width: 120px
-      padding-left: 12px
-      margin-top: auto
-
-    .ueid-container
-      margin-left: auto
-      text-align: right
-      font-size: 10px
-      line-height: 1
-      .ueid 
-        font-family: 'Inter', sans-serif
-        font-weight: 400
-        margin-bottom: 4px
-        span
-          font-weight: 800
-      .url a
-        color: hsl(270, 100%, 52%) !important
-        font-family: 'Inter-Black', sans-serif
-        &:hover
-          color: hsl(270, 100%, 24%) !important
-          text-decoration: none !important
-  .disclaimer
-    font-family: 'Inter', sans-serif
-    font-size: 11px
-    font-weight: 400
-    line-height: 1.4
-    color: hsl(200, 8%, 32%)
-
   .vertical .chart .answer, .horizontal .chart .answer
     display: flex
-
     font-weight: 500
     font-family: 'Inter-Medium', sans-serif
     letter-spacing: -0.02em
