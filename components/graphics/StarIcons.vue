5ee92ea60177246a5e987526c558b4727c6a0630
Max Burnside <maxburnside@gmail.com>
2021-09-17 10:31:08 -0700

    Getting star_gradient into a place where it won't break other things, though it is still not loading the gradient as expected

--- components/graphics/StarIcons.vue
+++ components/graphics/StarIcons.vue
@@ -1,6 +1,6 @@
 <template lang='pug'>
   .star_icons
-    Star100(v-for='i in star_count' :key='i')
+    Star100(v-for='i in star_count' :key='i' :style='star_gradient')
     component(:is='star_fraction')
 </template>
 <script>
@@ -9,8 +9,14 @@ import Star50 from './Star50'
 import Star75 from './Star75'
 import Star100 from './Star100'
 export default {
-  props: ['stars'],
+  props: ['stars', 'brand_color_1', 'brand_color_2'],
   components: { Star100, Star75, Star50, Star25 },
+  mounted() {
+    if(!this.content_asset.account.brand_color_1 == null)
+      this.content_asset.account.brand_color_1 = '#850AFF'
+    if(this.content_asset.account.brand_color_2 == null)
+      this.content_asset.account.brand_color_2 = '#850AFF'
+  },
   computed: {
     star_count() {
       return Math.floor(this.stars)
@@ -29,22 +35,26 @@ export default {
         return 'Star75'
       else
         return 'Star100'
-    }
-    
+    },
+    star_gradient() {
+      return {
+        fill: `linear-gradient(180deg, ${this.content_asset?.account?.brand_color_1} 0%, ${this.content_asset?.account?.brand_color_2} 100%)`
+      }
+    },
   },
 }
 </script>
 <style lang='sass'>
-  .star_icon
-    width: 32px
-    height: 32px
-    position: relative
+  .star_icons
+    margin-bottom: 16px
+    svg
+      height: 48px
+      width: 48px
+      &:not(:last-child)
+        margin-right: 8px
   .cover
     position: absolute
     background: white
     height: 32px
     right: 0
-  .star
-    position: absolute
-    border: 3px solid red
 </style>
