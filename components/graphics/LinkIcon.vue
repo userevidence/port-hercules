457b5bbe30e4f4f4b2bc9e20db008df60e9a2cfc
Max Burnside <maxburnside@gmail.com>
2021-09-29 07:54:29 -0700

    Created LinkIcon

---
+++ components/graphics/LinkIcon.vue
@@ -0,0 +1,7 @@
+<template lang='html'>
+    <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M5 11L11 5" stroke="#899EA9" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+        <circle cx="4.5" cy="11.5" r="3.5" stroke="#899EA9" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+        <circle cx="11.5" cy="4.5" r="3.5" stroke="#899EA9" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+    </svg>
+</template>
