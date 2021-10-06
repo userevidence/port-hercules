0bdac2efef48abcc1183bc6e34e2fc5e0bd9346e
Billy Fowks <fowkswe@gmail.com>
2021-10-06 09:46:59 -0500

    rework barguts to be complete percentage based

--- components/BarGuts.vue
+++ components/BarGuts.vue
@@ -1,11 +1,11 @@
 <template lang='pug'>
   .bar_guts(:class='orientation')
-    .chart(:class='orientation')
+    .chart
       .legend
         .legendy % of Users
-      .bar_group(v-for='(stat, i) in shown_stats' :class='barChartCount')
-        .channel
-          .bar(:class='statClass(i)' :style='barSize(stat.count)')
+      .bar_group(v-for='(stat, i) in shown_stats')
+        .channel(:style='barSize(stat.count)' :class='bar_class')
+          .bar(:class='statClass(i)')
             .stat 
               | {{statPercent(stat.count)}}
               span %
@@ -28,39 +28,22 @@ export default {
     }
   },
   computed: {
-    shortChart() {
+    short_chart() {
       return Math.max(...this.stats.map((stat) => Number(this.statPercent(stat.count)))) < 50
-    },
-    barChartCount() {
-      if (this.shown_stats.length > 9)
-        return 'many_bars'
-      else if (this.shown_stats.length == 9)
-        return 'nine_bars'
-      else if (this.shown_stats.length == 8)
-        return 'eight_bars'
-      else if (this.shown_stats.length == 7)
-        return 'seven_bars'
-      else if (this.shown_stats.length == 6)
-        return 'six_bars'
-      else if (this.shown_stats.length == 5)
-        return 'five_bars'
-      else if (this.shown_stats.length == 4)
-        return 'four_bars'
-      else if (this.shown_stats.length == 3)
-        return 'three_bars'
-      else if (this.shown_stats.length == 2)
-        return 'two_bars'
-      else if (this.shown_stats.length == 1)
-        return 'one_bar'
-      else
-        return
     },
     orientation() {
       return Math.max(...this.stats.map((s) => s.the_answer.length), 0) > 10 ? 'horizontal' : 'vertical'
     },
     shown_stats() { 
       return this.stats.filter(s => !s.exclude_from_asset)
-    }
+    },
+    max_percent() {
+      return Math.max(...this.stats.map((stat) => Number(this.statPercent(stat.count))))
+    },
+    bar_class() {
+      if(this.orientation == 'vertical') 
+        return this.stats.length > 6 ? 'barx' : `bar${this.stats.length}`
+    },
   },
   methods: {
     statPercent(value) {
@@ -69,18 +52,14 @@ export default {
     barSize(value) {
       if(this.orientation == 'horizontal') {
         var width = this.statPercent(value)
-        if(this.shortChart)
+        if(this.short_chart)
           width = Math.log10(Number(width))*50
         return 'width: ' + width + '%'
       }
         
       else {
         var height = Number(this.statPercent(value))
-        
-        if(this.shortChart)
-          height = height*3
-        return 'height: ' + 300 * height / 100 + 'px'
-        
+        return 'height: ' + height / this.max_percent * 100 + '%'
       }
     },
     statClass(value) {
@@ -94,6 +73,7 @@ export default {
 </script>
 <style lang='sass' scoped>
   .bar_guts
+    height: 100%
   .horizontal
     .chart
       .bar_group
@@ -109,39 +89,29 @@ export default {
         min-width: 48px
       .answer
         margin-top: 4px
+        width: 100%
       .stat
         left: 12px
     .legendy, .legendx
       display: none
   .vertical
     .chart
+      height: 100%
       display: flex
       justify-content: space-between
-      position: relative
-      .many_bars
-        width: 9%
-      .nine_bars
-        width: 10%
-      .eight_bars
-        width: 11%
-      .seven_bars
-        width: 13%
-      .six_bars
-        width: 14%
-      .five_bars
-        width: 18%
-      .four_bars
-        width: 23%
-      .three_bars
-        width: 31%
-      .two_bars
-        width: 48%
-      .one_bar
-        width: 100%
+      align-items: flex-end
+      margin-left: -1%
+      margin-right: -1%
       .bar_group
         display: flex
         flex-direction: column
         justify-content: flex-end
+        width: 100%
+        height: 100%
+        margin-left: 1%
+        margin-right: 1%
+        .channel
+          height: 100%
       .bar
         border-radius: 8px 8px 0px 0px
         padding: 0
@@ -188,26 +158,25 @@ export default {
       color: hsla(200, 100%, 100%, 0.8)
       position: relative
       left: 2px
-
-  .seven_bars .stat, .eight_bars .stat, .nine_bars .stat
+  .bar7 .stat, .bar8 .stat, .bar9 .stat
     bottom: -3px
     span
       font-size: 10px
-
-  .seven_bars .stat
+  
+  .bar7 .stat
     font-size: 20px
     span
       font-size: 12px
-
-  .eight_bars .stat
+  
+  .bar8 .stat
     font-size: 19px
     span
       font-size: 11px
-
-  .nine_bars .stat
+  
+  .bar9 .stat
     font-size: 18px
-
-  .many_bars .stat
+  
+  .barx .stat
     bottom: -2px
     font-size: 16px
     span
