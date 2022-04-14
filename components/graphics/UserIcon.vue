47e33ac2686d46693ba1a469eb3a5df7607270cb
Max Burnside <maxburnside@gmail.com>
2022-04-14 10:55:29 -0700

    Changed default color of UserIcon

--- components/graphics/UserIcon.vue
+++ components/graphics/UserIcon.vue
@@ -1,7 +1,7 @@
 <template>
     <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <circle cx="6" cy="2" r="2" :fill="color1" />
-        <path d="M1 12V10C1 7.79086 2.79086 6 5 6H7C9.20914 6 11 7.79086 11 10V12H1Z" :fill="color1" />
+        <circle cx="6" cy="2" r="2" fill='hsl(200, 24%, 80%)' />
+        <path d="M1 12V10C1 7.79086 2.79086 6 5 6H7C9.20914 6 11 7.79086 11 10V12H1Z" fill='hsl(200, 24%, 80%)' />
     </svg>
 </template>
 <script>
